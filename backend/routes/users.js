0f55517d165860cdc7cf4f6f28a339840c1deed2
kaltersia <74735042+kaltersia@users.noreply.github.com>
2021-06-21 13:59:21 +0200

    Update users.js

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -64,24 +64,12 @@ router.post("/register", userUpload.single("image"), async (req, res) => {
       <h3>Message</h3>
     `;
 
-    // create reusable transporter object using the default SMTP transport
-    // let transporter = nodemailer.createTransport({
-    //   host: "kaltersia.com",
-    //   port: 465,
-    //   secure: true, // true for 465, false for other ports
-    //   auth: {
-    //     user: "adi@kaltersia.com", // generated ethereal user
-    //     pass: "Tralala@666", // generated ethereal password
-    //   },
-    //   tls: {
-    //     rejectUnauthorized: false,
-    //   },
-    // });
+ 
 
     // setup email data with unicode symbols
     let mailOptions = {
-      from: '"NodeJs & Nodemailer" <adi@kaltersia.com>', // sender address
-      to: "adikica91@gmail.com", // list of receivers
+      from: '"NodeJs & Nodemailer" <someMail@SomeDomain.com>', // sender address
+      to: "receiver@Email.address", // list of receivers
       subject: "Regjistrim i ri", // Subject line
       text: "Hello world?", // plain text body
       html: output, // html body
