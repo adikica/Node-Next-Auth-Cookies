5be959c20b2030aed9903bb8f4fc5e2156e31fdf
kaltersia <74735042+kaltersia@users.noreply.github.com>
2021-06-21 14:00:43 +0200

    Update Node-Mailer.js

--- backend/middlewares/Node-Mailer.js
+++ backend/middlewares/Node-Mailer.js
@@ -13,19 +13,6 @@ const mailConfig = {
   },
 };
 
-// const mailConfig = {
-//   host: "kaltersia.com",
-//   port: 465,
-//   secure: true, // true for 465, false for other ports
-//   auth: {
-//     user: "adi@kaltersia.com", // generated ethereal user
-//     pass: "Tralala@666", // generated ethereal password
-//   },
-//   tls: {
-//     rejectUnauthorized: false,
-//   },
-// };
-
 var transporter = mailer.createTransport(mailConfig);
 
 module.exports = transporter;
